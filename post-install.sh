6ca8a3e306861321b76f48975beb267822cf5d15
Lucas <lucasvalenteds@gmail.com>
2022-04-09 18:10:28 -0300

    Creating post-install script
    It starts services and configure desktop environment.

---
+++ post-install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+enable_services() {
+  while IFS= read -r service
+  do
+    sudo systemctl enable "$service" --now
+  done < "services.txt"
+}
+
+configure_gnome() {
+  # Invert touchpad/mouse scrolling direction
+  gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
+  gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+
+  # Enable minimize and maximize window buttons
+  gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
+  # Enable touchpad tap-to-click
+  gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+
+  # Creating keyboard shortcut to open terminal using Ctrl+Alt+T
+  dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Open terminal'"
+  dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'gnome-terminal'"
+  dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Control><Alt>t'"
+
+  # Disable Activities hot corner
+  gsettings set org.gnome.desktop.interface enable-hot-corners false
+
+  # Set Gedit theme to Oblivion and change editor font
+  gsettings set org.gnome.gedit.preferences.editor scheme "oblivion"
+  gsettings set org.gnome.gedit.preferences.editor editor-font "Monospace 12"
+  gsettings set org.gnome.gedit.preferences.editor use-default-font false
+  gsettings set org.gnome.gedit.preferences.editor tabs-size "uint32 4"
+}
+
+enable_services
+configure_gnome
